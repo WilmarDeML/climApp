a50ee175c9329544662516dd60744be0d08e7a65
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-07-31 04:47:48 -0500

    Create index.js for About Component

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  About = () => {
+	return (
+		<section className='text bg-dark text-light'>
+			<h2 className='text-success'>About ClimApp</h2>
+		</section>
+	)
+}
+
+export default  About;
