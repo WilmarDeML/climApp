ecab68a0b67cec6fb4b5b6c2d01f8567971c7607
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-07-31 04:49:57 -0500

    Create index.js for Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar'
+import Cards from '../Cities'
+
+const Home = () => (
+    <>
+        <NavBar />
+        <Cards />
+    </>
+)
+
+export default Home
