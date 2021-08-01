125e6755a81473ae14e87806e47527b447c98348
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-07-31 22:22:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {Route} from 'react-router-dom'
-import Home from './components/Home';
-import About from './components/About';
+import Home from './components/Home'
+import About from './components/About'
+import CityDetail from './components/CityDetail'
 import './App.css'
 
 const App = () => (
   <div className="App">
     <Route exact path='/' component={Home} />
     <Route exact path='/about' component={About} />
-    {/*<Route exact path='/countries/:id' component={DetallePais} /> */}
+    <Route exact path='/city/:id' component={CityDetail} />
   </div>
 );
 
